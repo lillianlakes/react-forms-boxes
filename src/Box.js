74b49edfeaa4cda2b0456b104e2c3bf2be556d5b
lillianlakes <lillian.lakes@gmail.com>
2021-08-12 12:35:20 -0700

    completed removeBox and had code review

--- src/Box.js
+++ src/Box.js
@@ -1,11 +1,15 @@
-function Box({width, height, backgroundColor}) {
+function Box({id, width, height, backgroundColor, removeBox}) {
     width = `${width}px`;
     height = `${height}px`;
 
     return (
-    <div 
-        className="Box"
-        style={{ width, height, backgroundColor }}>
+    <div className="Box">
+      <div 
+          className="Box-component" style={{ width, height, backgroundColor }}>
+      </div>
+      <div className="Box-removeBox">
+        <button onClick={() => removeBox(id)}>Remove the Box</button>
+      </div>
     </div>
     );
 }
--- src/BoxList.js
+++ src/BoxList.js
@@ -9,30 +9,39 @@ import { v4 as uuid } from 'uuid';
  * State:
  * - boxes: array like [ { id, width, height, backgroundColor }, ... ]
  *
- * BoxList --> 
+ * BoxList --> NewBoxForm
  */
 
 function BoxList() {
   const [boxes, setBoxes] = useState([]);
   console.log(boxes);
 
+  /** Add new Box to boxes list. */
+  function addBox(box) {
+    let newBox = { ...box, id: uuid() };
+    setBoxes(boxes => [...boxes, newBox]);
+  }
+  // end
+  
+  /** Remove box from boxes list */
+  function removeBox(id) {
+    setBoxes(boxes => boxes.filter(box => box.id !== id));
+  }
+  
   function renderBoxes() {
     return (
         <ul>
           {boxes.map(box => (
-            //   <li key={box.id}>
-                <Box width={box.width} height={box.height} backgroundColor={box.backgroundColor}/>
-            //   </li>
+                <Box id = {box.id} 
+                key = {box.id} 
+                width={box.width} 
+                height={box.height} 
+                backgroundColor={box.backgroundColor} 
+                removeBox = {removeBox}
+                />
           ))}
         </ul>
     );
-  }
-  // end
-
-  /** Add new Box object to cart. */
-  function addBox(box) {
-    let newBox = { ...box, id: uuid() };
-    setBoxes(boxes => [...boxes, newBox]);
   }
   // end
 
@@ -41,7 +50,7 @@ function BoxList() {
       <NewBoxForm addBox={addBox} />
       {renderBoxes()}
     </div>
-  );
+  );  
 }
 // end
 
